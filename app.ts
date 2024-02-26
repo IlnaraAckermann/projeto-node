21a523d3b71cebdbc0d86ab256ceb373ca193735
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-02-26 09:00:59 -0300

    feat(prisma): Configurações iniciais do Prisma

--- app.ts
+++ app.ts
@@ -1,18 +1,15 @@
-const express = require("express");
+import express from "express";
 import "dotenv/config";
 
-const app= express();
+const app = express();
 
-
-
-app.get("/", (req:any, res:any)=>{ 
-    res.status(200)
-        .send("oi");
+app.get("/", (req, res) => {
+    res.status(200).send("oi");
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Servidor rodando em http://localhost:${port}/`);    
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}/`);
 });
 // sdsds
