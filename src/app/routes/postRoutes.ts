b9afe30eac7de7709ad72f27fdbb8aba31a28e77
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-03-04 15:05:43 -0300

    rotas basica da api

--- src/app/routes/postRoutes.ts
+++ src/app/routes/postRoutes.ts
@@ -1,8 +1,24 @@
 import { Router } from "express";
 const postRouter = Router();
 
-postRouter.get("/", (req, res) => {
-  res.send("Rota de post");
-});
+postRouter
+  .get("/", (req, res) => {
+    res.send("Rota de post /");
+  })
+  .post("/", (req, res) => {
+    res.send("post de post /");
+  })
+  .get("/:id", (req, res) => {
+    res.send("get de post /:id");
+  })
+  .put("/:id", (req, res) => {
+    res.send("put de post /:id");
+  })
+  .patch("/:id", (req, res) => {
+    res.send("patch de post /:id");
+  })
+  .delete("/:id", (req, res) => {
+    res.send("delete de post /:id");
+  });
 
 export { postRouter };
--- src/app/routes/userRoutes.ts
+++ src/app/routes/userRoutes.ts
@@ -2,8 +2,24 @@ import { Router } from "express";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-  res.send("Rota de user");
-});
+userRouter
+  .get("/", (req, res) => {
+    res.send("Rota de user /");
+  })
+  .post("/", (req, res) => {
+    res.send("post de user /");
+  })
+  .get("/:id", (req, res) => {
+    res.send("get de user /:id");
+  })
+  .put("/:id", (req, res) => {
+    res.send("put de user /:id");
+  })
+  .patch("/:id", (req, res) => {
+    res.send("patch de user /:id");
+  })
+  .delete("/:id", (req, res) => {
+    res.send("delete de user /:id");
+  });
 
 export { userRouter };
