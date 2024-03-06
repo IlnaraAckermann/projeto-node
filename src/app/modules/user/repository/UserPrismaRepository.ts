7e92007481b62d99dde855abe31fb314623de66f
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-03-05 23:39:52 -0300

    Feat: adicionado repository que utiliza o prisma

---
+++ src/app/modules/user/repository/UserPrismaRepository.ts
@@ -0,0 +1,59 @@
+import { UserGetRecord } from "../models/UserGetRecord";
+import { UserPostRecord } from "../models/UserPostRecord";
+import { UserUpdatedRecord } from "../models/UserUpdatedRecord";
+import { IUserRepository } from "./IUserRepository";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export class UserPrismaRepository implements IUserRepository {
+  async getAll(): Promise<UserGetRecord[]> {
+    const users: UserGetRecord[] = await prisma.user.findMany({
+      include: {
+        posts: true, // Returns all fields for all posts
+        profile: true, // Returns all Profile fields
+      },
+    });
+    return users;
+  }
+  async post(userDto: UserPostRecord): Promise<unknown> {
+    const { email, name } = userDto;
+    return await prisma.user.create({
+      data: {
+        email,
+        name,
+      },
+    });
+  }
+  async getById(id: number): Promise<UserGetRecord> {
+    const result = await prisma.user.findUniqueOrThrow({
+      include: {
+        posts: true, // Returns all fields for all posts
+        profile: true, // Returns all Profile fields
+      },
+      where: {
+        id: id,
+      },
+    });
+    return result;
+  }
+  async update(
+    id: number,
+    userDto: UserPostRecord,
+  ): Promise<UserUpdatedRecord> {
+    const { email, name } = userDto;
+    await prisma.user.update({
+      where: { id: id },
+      data: {
+        email,
+        name,
+      },
+    });
+    const userUpdated: UserUpdatedRecord = { email, name, id };
+    return userUpdated;
+  }
+  async delete(id: number): Promise<unknown> {
+    return await prisma.user.delete({
+      where: { id: id },
+    });
+  }
+}
