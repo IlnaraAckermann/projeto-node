24b0a8e51ce1c6ba849c3e9417b2ea653520118e
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-03-05 23:43:45 -0300

    User Routes adptado para utilizar o usercontroller do factory

--- src/app/routes/userRoutes.ts
+++ src/app/routes/userRoutes.ts
@@ -1,25 +1,13 @@
+import { userController } from "@modules/user/factory/userFactory";
 import { Router } from "express";
 
 const userRouter = Router();
 
 userRouter
-  .get("/", (req, res) => {
-    res.send("Rota de user /");
-  })
-  .post("/", (req, res) => {
-    res.send("post de user /");
-  })
-  .get("/:id", (req, res) => {
-    res.send("get de user /:id");
-  })
-  .put("/:id", (req, res) => {
-    res.send("put de user /:id");
-  })
-  .patch("/:id", (req, res) => {
-    res.send("patch de user /:id");
-  })
-  .delete("/:id", (req, res) => {
-    res.send("delete de user /:id");
-  });
+  .get("/", userController.getAll.bind(userController))
+  .post("/", userController.post.bind(userController))
+  .get("/:id", userController.getById.bind(userController))
+  .put("/:id", userController.update.bind(userController))
+  .delete("/:id", userController.delete.bind(userController));
 
 export { userRouter };
