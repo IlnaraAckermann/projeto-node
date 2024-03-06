2257adf51f487164f01998b0dc6d4419c93fdf76
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-03-06 10:30:21 -0300

    Atualiza rotas para o profileRoutes
    Este arquivo define as rotas para os endpoints relacionados aos perfis:
    - GET /profiles: Retorna todos os perfis.
    - POST /profiles: Cria um novo perfil.
    - GET /profiles/:id: Retorna um perfil por ID.
    - PUT /profiles/:id: Atualiza um perfil existente.
    - DELETE /profiles/:id: Exclui um perfil por ID.
    
    Cada rota chama o método correspondente do ProfileController.

--- src/app/routes/profileRoutes.ts
+++ src/app/routes/profileRoutes.ts
@@ -1,25 +1,13 @@
+import { profilerController } from "@modules/profile/factory/ProfileFactory";
 import { Router } from "express";
 
 const profileRouter = Router();
 
 profileRouter
-  .get("/", (req, res) => {
-    res.send("Rota de profile /");
-  })
-  .post("/", (req, res) => {
-    res.send("post de profile /");
-  })
-  .get("/:id", (req, res) => {
-    res.send("get de profile /:id");
-  })
-  .put("/:id", (req, res) => {
-    res.send("put de profile /:id");
-  })
-  .patch("/:id", (req, res) => {
-    res.send("patch de profile /:id");
-  })
-  .delete("/:id", (req, res) => {
-    res.send("delete de profile /:id");
-  });
+  .get("/", profilerController.getAll.bind(profilerController))
+  .post("/", profilerController.post.bind(profilerController))
+  .get("/:id", profilerController.getById.bind(profilerController))
+  .put("/:id", profilerController.update.bind(profilerController))
+  .delete("/:id", profilerController.delete.bind(profilerController));
 
 export { profileRouter };
