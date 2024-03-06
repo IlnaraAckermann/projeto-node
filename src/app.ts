93a166ccc4ee40ed6d9f1ef0b62296fd99dd4e03
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-03-05 23:44:44 -0300

    removido imports desnecessáros

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { indexRoute } from "./app/routes/indexRouter";
-import { PrismaClient } from "@prisma/client";
 import { errorHandlerMiddleware } from "@middlewares/index";
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use([cors(), express.json()]);
 app.use("/", indexRoute);
