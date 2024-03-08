d6dbdebbaa92fc628ce9a8dddf1d80624ce6a21c
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-03-08 10:27:26 -0300

    configurações iniciais da documentação swagger

--- src/app/routes/indexRouter.ts
+++ src/app/routes/indexRouter.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { postRouter } from "./postRoutes";
 import { userRouter } from "./userRoutes";
 import { profileRouter } from "./profileRoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "@swagger/swagger.json";
 
 const indexRoute = Router();
 
 indexRoute.get("/", (req, res) => {
   res.send("Rota basica");
 });
+indexRoute.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 indexRoute.use("/posts", postRouter);
 indexRoute.use("/users", userRouter);
 indexRoute.use("/profiles", profileRouter);
